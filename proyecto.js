8c0720072116a5676c68569e0e45347fb79f58f6
pablo <zeus@gmail.com>
2024-09-16 10:53:24 -0400

    loco

--- proyecto.js
+++ proyecto.js
@@ -3,3 +3,9 @@ console.log("hello word")
 console.log("Hola hector")
 
 console.log("Estoy en informatica porque me gusta jugar lol")
+
+console.log("hello Disney")
+
+console.log("Hola Guys")
+
+console.log("Estoy en informatica porque me gusta jugar lol")
