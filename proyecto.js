7990de50554856ff60f8aab587ef5d1671f28206
jhosnar ereu <jhosnar502@gmail.com>
2024-09-16 10:27:45 -0400

    sunma

--- proyecto.js
+++ proyecto.js
@@ -1,8 +1,6 @@
 console.log("hello word")
 
-console.log("Hola hector")
-
-console.log("Estoy estudiando informatica por que me gustan las computadoras")
-console.log("Estoy en informatica porque me gusta jugar lol")
-
 console.log("Estoy programando en la rama articulo")  
+suma = 0;
+suma = 1 + 2
+console.log(suma); 
