3bd02710ff0555d0aa5f59f9cfb57370a67cef8f
jhosnar ereu <jhosnar502@gmail.com>
2024-09-16 10:22:49 -0400

    articulo

--- proyecto.js
+++ proyecto.js
@@ -4,3 +4,5 @@ console.log("Hola hector")
 
 console.log("Estoy estudiando informatica por que me gustan las computadoras")
 console.log("Estoy en informatica porque me gusta jugar lol")
+
+console.log("Estoy programando en la rama articulo")  
